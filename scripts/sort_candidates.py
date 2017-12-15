97e73b3e8593bb17596f3db4f78ca2f0235ffa84
pdtrang <phamdiemtrang1987@gmail.com>
2017-12-14 22:26:41 -0600

    rearrange scripts

--- scripts/sort_candidates.py
+++
@@ -1,8 +0,0 @@
-import sys
-import pandas
-if len(sys.argv) != 2:
-	print("Usage:", sys.argv[0] + "  input.csv")
-else:
-	data = pandas.read_csv(sys.argv[1])
-	data = data.sort_values('Score', ascending=False)
-	data.to_csv(sys.argv[1])
