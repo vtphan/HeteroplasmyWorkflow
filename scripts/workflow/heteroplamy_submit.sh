c9f1c4de01cb4223aa985f3e03651ee93e6550a4
pdtrang <phamdiemtrang1987@gmail.com>
2017-12-21 19:00:33 -0600

    add codes to clean up hpc output files in scripts dir

--- scripts/workflow/heteroplamy_submit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh 
-#PBS -l nodes=1:default:ppn=1 
-#PBS -l walltime=72:00:00 
-#PBS -N HTPLASMY_JOB#PBS -t 0-1 
-cd /home/dpham2/carrot/scripts 
-python hpc_align.py ../config.txt readids${PBS_ARRAYID}.txt 
